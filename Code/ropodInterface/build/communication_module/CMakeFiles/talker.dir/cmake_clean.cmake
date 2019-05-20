7e983ae5d9c1de04953b8cc05d9b82ea640601fa
prites Gohil <priteshbgohil@gmail.com>
2019-05-20 16:33:20 +0200

    adding ros package and talker in cpp file for our communication code

---
+++ Code/ropodInterface/build/communication_module/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/pritesh/Desktop/sem2/sdp/sdp_ss2019_P6_IntentionComm/Code/ropodInterface/devel/lib/communication_module/talker.pdb"
+  "/home/pritesh/Desktop/sem2/sdp/sdp_ss2019_P6_IntentionComm/Code/ropodInterface/devel/lib/communication_module/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
