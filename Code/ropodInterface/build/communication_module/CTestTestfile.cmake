c5e68cfd15c6af72361f9d5f320ff4f436a2707c
prites Gohil <priteshbgohil@gmail.com>
2019-05-23 17:35:04 +0200

    Remove the now ignored directory node_modules

--- Code/ropodInterface/build/communication_module/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pritesh/Desktop/sem2/sdp/sdp_ss2019_P6_IntentionComm/Code/ropodInterface/src/communication_module
-# Build directory: /home/pritesh/Desktop/sem2/sdp/sdp_ss2019_P6_IntentionComm/Code/ropodInterface/build/communication_module
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
